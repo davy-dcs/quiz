69bddef998a8ebeb602ab7fe4a7d0007599de20f
Davy Descamps <davydescamps01@gmail.com>
2025-02-11 13:38:43 +0100

    Methode post de SessionService tests

--- src/test/java/fr/acajou/quiz/service/impl/SessionServiceImplTest.java
+++ src/test/java/fr/acajou/quiz/service/impl/SessionServiceImplTest.java
@@ -2,13 +2,17 @@ package fr.acajou.quiz.service.impl;
 
 import fr.acajou.quiz.domain.Quiz;
 import fr.acajou.quiz.domain.Role;
+import fr.acajou.quiz.domain.Session;
 import fr.acajou.quiz.domain.Users;
+import fr.acajou.quiz.dto.session.ISessionMapper;
 import fr.acajou.quiz.dto.session.SessionRequest;
 import fr.acajou.quiz.dto.session.SessionResponse;
 import fr.acajou.quiz.exception.QuizNotFoundException;
 import fr.acajou.quiz.exception.SessionConflictException;
 import fr.acajou.quiz.exception.UserNotFoundException;
+import fr.acajou.quiz.repository.IQuizRepository;
 import fr.acajou.quiz.repository.ISessionRepository;
+import fr.acajou.quiz.repository.IUserRepository;
 import fr.acajou.quiz.service.IQuizService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,58 +21,84 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static fr.acajou.quiz.domain.Category.HISTORY;
 import static fr.acajou.quiz.domain.Difficulty.EASY;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 @Slf4j
 @Disabled
 class SessionServiceImplTest {
-    @InjectMocks
-    private SessionServiceImpl sessionService;
-    @Mock
-    private ISessionRepository sessionRepository;
     @Mock
     private UserDetailsServiceImpl userService;
-    @Mock
-    private IQuizService quizService;
 
-    private Users user;
-    private Quiz quiz;
+    @Mock
+    private QuizServiceImpl quizService;
+
+    @Mock
+    private ISessionRepository sessionRepository;
+
+    @Mock
+    private ISessionMapper sessionMapper;
+
+    @InjectMocks
+    private SessionServiceImpl sessionService;
+
+
+    private Users mockUser;
+    private Quiz mockQuiz;
     private SessionRequest sessionRequest;
+    private Session mockSession;
+    private SessionResponse sessionResponse;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        user = new Users(5L, UUID.randomUUID(), "username", "password", new Role("ADMIN"));
-        quiz = Quiz.builder()
+        mockUser = Users.builder()
+                .username("test")
+                .password("test")
+                .role(new Role("ROLE_ADMIN"))
+                .build();
+        mockQuiz = Quiz.builder()
                 .title("titre")
                 .description("description")
                 .category(HISTORY)
                 .difficulty(EASY).build();
-        sessionRequest = new SessionRequest(null, UUID.randomUUID(), UUID.randomUUID());
+        sessionRequest = new SessionRequest(null, mockQuiz.getUuid(), mockUser.getUuid());
+        mockSession = Session.builder()
+                .timer(null)
+                .user(mockUser)
+                .quiz(mockQuiz)
+                .build();
+        sessionResponse = new SessionResponse(mockSession.getTimer(), mockSession.getUuid(), mockSession.getDate(), mockSession.getQuiz().getUuid(), mockSession.getUser().getUuid());
     }
 
 
     @Test
     void shouldReturnSessionConflictException() {
         //given
-        when(userService.getUser(user.getUuid())).thenReturn(user);
-        when(quizService.getQuiz(quiz.getUuid())).thenReturn(quiz);
-        given(sessionRepository.existsByTimerAndQuiz_UuidAndUser_Uuid(sessionRequest.timer(), sessionRequest.quiz(), sessionRequest.user())).willReturn(true);
+        when(userService.getUser(sessionRequest.user())).thenReturn(mockUser);
+        when(quizService.getQuiz(sessionRequest.quiz())).thenReturn(mockQuiz);
+        when(sessionRepository.existsByTimerAndQuiz_UuidAndUser_Uuid(sessionRequest.timer(), mockQuiz.getUuid(), mockUser.getUuid())).thenReturn(true);
 
         //then
-        assertThrows(SessionConflictException.class, () -> sessionService.post(sessionRequest));
+        assertThrows(SessionConflictException.class, () -> {sessionService.post(sessionRequest);});
     }
 
     @Test
     void shouldReturnQuizNotFoundException() {
         //given
-        given(sessionService.post(sessionRequest)).willThrow(QuizNotFoundException.class);
+        when(userService.getUser(sessionRequest.user())).thenReturn(mockUser);
+        when(quizService.getQuiz(sessionRequest.quiz())).thenThrow(QuizNotFoundException.class);
+
         //then
         assertThrows(QuizNotFoundException.class, () -> sessionService.post(sessionRequest));
     }
@@ -76,7 +106,8 @@ class SessionServiceImplTest {
     @Test
     void shouldReturnUserNotFoundException() {
         //given
-        given(sessionService.post(sessionRequest)).willThrow(UserNotFoundException.class);
+        when(userService.getUser(sessionRequest.user())).thenThrow(UserNotFoundException.class);
+
         //then
         assertThrows(UserNotFoundException.class, () -> sessionService.post(sessionRequest));
     }
@@ -84,9 +115,11 @@ class SessionServiceImplTest {
     @Test
     void shouldReturnSessionResponse() {
         //given
-        given(userService.getUser(user.getUuid())).willReturn(user);
-        given(quizService.getQuiz(quiz.getUuid())).willReturn(quiz);
-        given(sessionRepository.existsByTimerAndQuiz_UuidAndUser_Uuid(null, quiz.getUuid(), user.getUuid())).willReturn(false);
+        when(userService.getUser(sessionRequest.user())).thenReturn(mockUser);
+        when(quizService.getQuiz(sessionRequest.quiz())).thenReturn(mockQuiz);
+        when(sessionRepository.existsByTimerAndQuiz_UuidAndUser_Uuid(sessionRequest.timer(), mockQuiz.getUuid(), mockUser.getUuid())).thenReturn(false);
+        when(sessionRepository.save(any(Session.class))).thenReturn(mockSession);
+        when(sessionMapper.sessionToSessionResponse(any(Session.class))).thenReturn(sessionResponse);
 
         //when
         final SessionResponse actual = sessionService.post(sessionRequest);
